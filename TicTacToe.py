b33f685b4f80d94b4dc3a936570d169ffed3b314
ldm04 <62068653+ldm04@users.noreply.github.com>
2020-04-29 22:29:17 -0500

    Add files via upload

--- TicTacToe.py
+++ TicTacToe.py
@@ -50,7 +50,7 @@ class Game(Canvas):
         d2.configure(width = 25, bg = "white", fg = "black", font = ("Helvetica", 10))
         d2_window = canvas.create_window(w/4, 43*h/64, anchor = CENTER, window = d2)
 
-        d3 = Label(self, text = "Every player plays X and the")
+        d3 = Label(self, text = "Like traditional Tic-Tac-Toe, except")
         d3.configure(width = 25, bg = "white", fg = "black", font = ("Helvetica", 10))
         d3_window = canvas.create_window(3*w/4, 5*h/8, anchor = CENTER, window = d3)
 
@@ -111,25 +111,23 @@ class Game(Canvas):
         s9 = self.createSquare(5*w/6-5, 10+5*h/6, game, canvas, "s9")
 
         # cpu plays in top left corner always on first turn
-        self.playX(s1, "red", "s1", canvas, game, w, h)
+        if game == "Tic-Tac-Toe":
+            self.playX(s1, "red", "s1", canvas, game, w, h)
+        else:
+            self.playX(s5, "red", "s5", canvas, game, w, h)
 
     # creates squares as blank buttons
     def createSquare(self, x, y, game, canvas, dictS):
-        if game == "Tic-Tac-Toe":
-            s = Button(self, text = " ", width = 4, height = 1,\
-                       command = lambda: self.playX(s, "red", dictS, canvas, game, x, y)\
-                       if (self.turn%2 == 0) else self.playO(s, "blue", dictS, canvas, game, x, y))
-        else:
-            s = Button(self, text = " ", width = 4, height = 1,\
-                        command = lambda: self.playX(s, "red", dictS, canvas, game, x, y))
+        s = Button(self, text = " ", width = 4, height = 1,\
+                    command = lambda: self.playO(s, "blue", dictS, canvas, game, x, y))
         s.configure(fg = "white", bg = "white", relief = FLAT,\
                     font = ("Helvetica", 45))
         s_window = canvas.create_window(x, y, anchor = CENTER, window = s)
 
         return s
 
-    # the cpu plays by filling in button with X
-    def playCPU(self, canvas, game, w, h):
+    # the cpu plays by filling in button with X for Tic-Tac-Toe
+    def playCPUT(self, canvas, game, w, h):
         # finds where to play on cpu's second turn
         if self.turn == 2:
             if (self.spaces["s2"] == "O") or (self.spaces["s4"] == "O")\
@@ -249,23 +247,51 @@ class Game(Canvas):
                 self.playX(s6, "red", "s6", canvas, game, w, h)
             else:
                 self.playX(s8, "red", "s8", canvas, game, w, h)
+
+    # the cpu plays by filling in button with X for Tic-Tac-Toe
+    def playCPUM(self, canvas, game, w, h, dictS):
+        if dictS == "s1":
+            self.playX(s9, "red", "s9", canvas, game, w, h)
+        elif dictS == "s2":
+            self.playX(s8, "red", "s8", canvas, game, w, h)
+        elif dictS == "s3":
+            self.playX(s7, "red", "s7", canvas, game, w, h)
+        elif dictS == "s4":
+            self.playX(s6, "red", "s6", canvas, game, w, h)
+        elif dictS == "s6":
+            self.playX(s4, "red", "s4", canvas, game, w, h)
+        elif dictS == "s7":
+            self.playX(s3, "red", "s3", canvas, game, w, h)
+        elif dictS == "s8":
+            self.playX(s2, "red", "s2", canvas, game, w, h)
+        else:
+            self.playX(s1, "red", "s1", canvas, game, w, h)
                 
     # fills in button with X
     def playX(self, s, color, dictS, canvas, game, w, h):
         s.configure(text = "X", disabledforeground = color)
         s["state"] = DISABLED
         self.spaces[dictS] = "X"
-        self.checkWin(dictS, canvas, game, w, h)
-        self.turn += 1
+        state = self.checkWin(dictS, canvas, game, w, h)
+        if (state == "win" or state == "tie"):
+            self.replay(canvas, game, w, h)
+        else:
+            self.turn += 1
 
     # fills in button with O
     def playO(self, s, color, dictS, canvas, game, w, h):
         s.configure(text = "O", disabledforeground = color)
         s["state"] = DISABLED
         self.spaces[dictS] = "O"
-        self.checkWin(dictS, canvas, game, w, h)
-        self.turn += 1
-        self.playCPU(canvas, game, w, h)
+        state = self.checkWin(dictS, canvas, game, w, h)
+        if (state == "win" or state == "tie"):
+            self.replay(canvas, game, w, h)
+        else:
+            self.turn += 1
+            if (game == "Misere" and self.turn%2 == 0):
+                self.playCPUM(canvas, game, w, h, dictS)
+            else:
+                self.playCPUT(canvas, game, w, h)
 
     # check for win
     def checkWin(self, dictS, canvas, game, w, h):
@@ -283,31 +309,32 @@ class Game(Canvas):
                     result_window = canvas.create_window(250, 250, anchor = CENTER, window = result)
                 else:
                     self.userPoints += 1
-                    self.cpuPoints += 1
                     result = Label(self, text = "YOU WIN")
                     result.configure(width = 8, bg = "white", fg = "blue", font = ("Helvetica", 30))
                     result_window = canvas.create_window(250, 250, anchor = CENTER, window = result)
             else:
                 if self.turn%2 == 0:
-                    self.cpuPoints += 1
+                    self.userPoints += 1
                     result = Label(self, text = "THE CPU LOSES")
-                    result.configure(width = 14, bg = "white", fg = "red", font = ("Helvetica", 30))
+                    result.configure(width = 14, bg = "white", fg = "blue", font = ("Helvetica", 30))
                     result_window = canvas.create_window(250, 250, anchor = CENTER, window = result)
                 else:
-                    self.userPoints += 1
                     self.cpuPoints += 1
                     result = Label(self, text = "YOU LOSE")
-                    result.configure(width = 9, bg = "white", fg = "blue", font = ("Helvetica", 30))
+                    result.configure(width = 9, bg = "white", fg = "red", font = ("Helvetica", 30))
                     result_window = canvas.create_window(250, 250, anchor = CENTER, window = result)
         else:
             if " " in self.spaces.values():
                 pass
             else:
+                state = "tie"
                 self.ties += 1
                 result = Label(self, text = "IT'S A TIE")
                 result.configure(width = 9, bg = "white", fg = "black", font = ("Helvetica", 30))
                 result_window = canvas.create_window(250, 250, anchor = CENTER, window = result)
 
+        return state
+    
     # find three in a row
     def findThreeInRow(self, dictS, value):
         # check all possible three in a rows with space one
@@ -380,7 +407,17 @@ class Game(Canvas):
                 state = "na"
 
         return state
-        
+
+    # asks user if they want to replay
+    # not functional yet
+    def replay(self, canvas, game, w, h):
+##        o1 = Button(self, text = "Play Again", width = 10, height = 1,\
+##                    command = lambda: self.tictactoe(w, h, canvas))
+##        o1.configure(fg = "black", bg = "white", relief = FLAT,\
+##                    font = ("Helvetica", 30))
+##        o1_window = canvas.create_window(w/2, 3*h/4, anchor = CENTER, window = o1)
+        pass
+    
 # dimensions of the GUI
 WIDTH = 500
 HEIGHT = 500
